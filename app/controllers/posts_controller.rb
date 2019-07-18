6769f7c44307391c873ac5dd39d06a6c0f062562
poignant-minifier-0983 <davidfano@gmail.com>
2019-07-18 01:29:44 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,12 +3,9 @@ class PostsController < ApplicationController
     @posts = Post.all
   end
 
-  def new
-  end
-
   def create
     Post.create(title: params[:post][:title], description: params[:post][:description])
-    redirect_to_posts_path
+    redirect_to posts_path
   end
-
+  
 end
